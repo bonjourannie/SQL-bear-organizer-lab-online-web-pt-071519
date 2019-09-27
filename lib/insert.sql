d5af07b03347ea43df7db26ba0a0b693cb08bb28
Annie Truex <artruex@gmail.com>
2019-09-27 01:51:32 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "F", "red", "calm", 1);
-INSERT INTO bears VALUES (2,"Rowdy", 40, "F", "black", "intense", 1);
-INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "neon", "good", 0);
-INSERT INTO bears VALUES (4,"Sargeant Brown", 73, "M", "brown", "aggro", 1);
-INSERT INTO bears VALUES (5,"Melissa", 13, "F", "glitter", "dopey", 1);
-INSERT INTO bears VALUES (6,"Grinch", 18, "M", "green", "mean one", 1);
-INSERT INTO bears VALUES (7,"Wendy", 6, "F", "white", "jolly", 0);
-INSERT INTO bears VALUES (8,null, 2, "M", "red", "lethal", 0);
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
