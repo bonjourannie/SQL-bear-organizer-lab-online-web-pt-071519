0e4440ad540c9e608272d0aee27e69918e252fc1
Annie Truex <artruex@gmail.com>
2019-09-27 01:48:52 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "F", "red", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 40, "F", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 50, "F", "neon", "good", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 73, "M", "brown", "aggro", 1);
+INSERT INTO bears VALUES (5,"Melissa", 3, "F", "glitter", "dopey", 1);
+INSERT INTO bears VALUES (6,"Grinch", 18, "M", "green", "mean one", 1);
+INSERT INTO bears VALUES (7,"Wendy", 1, "F", "white", "jolly", 0);
+INSERT INTO bears VALUES (8,null, 2, "M", "red", "lethal", 0);
